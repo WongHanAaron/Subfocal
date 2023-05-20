6a245252eda1b3e30949e82e0c58944c0a6bcbfb
AaronWong <aaronwonghan@gmail.com>
2023-05-20 08:34:48 -0700

    Added stub unit test for Configurable.Tests.cpp

---
+++ tests/Subfocal.Core.UnitTests/Models/Configurable.Tests.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../../../src/Subfocal.Core/Utilities/Image/Montage.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SubfocalCoreUnitTests
+{
+	TEST_CLASS(Configurable_Tests)
+	{
+	public:
+		TEST_METHOD(SetConfiguration_Tests)
+		{
+
+		}
+	};
+}
