c5581924cdf17491b51596d6cb2541084f22d94d
AaronWong <aaronwonghan@gmail.com>
2023-06-23 23:21:10 -0700

    Initial development for edge detection

--- src/Subfocal.Core/Utilities/Image/Edge.cpp
+++ src/Subfocal.Core/Utilities/Image/Edge.cpp
@@ -3,9 +3,44 @@
 
 std::tuple<cv::Mat, cv::Mat> Edge::Sobel(cv::Mat image, int outputDepth, int kernelSize, int borderType)
 {
+	if (image.channels() > 1)
+	{
+		cv::Mat* images = new cv::Mat[image.channels()];
+		cv::Mat* xEdges = new cv::Mat[image.channels()];
+		cv::Mat* yEdges = new cv::Mat[image.channels()];
+		
+		cv::split(image, images);
+
+		for (int i = 0; i < image.channels(); i++)
+		{
+			auto singleImage = images[i];
+			auto edges = SobelSingle(singleImage, outputDepth, kernelSize, borderType);
+			xEdges[i] = std::get<0>(edges);
+			yEdges[i] = std::get<1>(edges);
+		}
+
+		cv::Mat mergedX;
+		cv::Mat mergedY;
+		cv::merge(xEdges, image.channels(), mergedX);
+		cv::merge(yEdges, image.channels(), mergedY);
+
+		delete images;
+		delete xEdges;
+		delete yEdges;
+
+		return std::make_tuple(mergedX, mergedY);
+	}
+	else
+	{
+		return SobelSingle(image, outputDepth, kernelSize, borderType);
+	}
+}
+
+std::tuple<cv::Mat, cv::Mat> Edge::SobelSingle(cv::Mat image, int outputDepth, int kernelSize, int borderType)
+{
 	cv::Mat grad_x, grad_y;
 	cv::Mat abs_grad_x, abs_grad_y;
-	
+
 	cv::Sobel(image, grad_x, outputDepth, 1, 0, kernelSize, 1.0, 0.0, borderType);
 	cv::Sobel(image, grad_y, outputDepth, 0, 1, kernelSize, 1.0, 0.0, borderType);
 
--- src/Subfocal.Core/Utilities/Image/Edge.hpp
+++ src/Subfocal.Core/Utilities/Image/Edge.hpp
@@ -5,4 +5,6 @@ class Edge
 {
 public:
 	static std::tuple<cv::Mat, cv::Mat> Sobel(cv::Mat image, int outputDepth, int kernelSize, int borderType = cv::BorderTypes::BORDER_DEFAULT);
+
+	static std::tuple<cv::Mat, cv::Mat> SobelSingle(cv::Mat image, int outputDepth, int kernelSize, int borderType = cv::BorderTypes::BORDER_DEFAULT);
 };
